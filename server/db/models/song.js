5cea8d83c48c21a6cb76296066a3d8f9c06d07a3
Karen MacPherson <kam0112@gmail.com>
2015-10-02 12:15:15 -0400

    schema file skeleton

---
+++ server/db/models/song.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+
+
+});
+
+mongoose.model('Song', schema);
---
+++ server/db/models/stepChart.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+
+
+});
+
+mongoose.model('StepChart', schema);
