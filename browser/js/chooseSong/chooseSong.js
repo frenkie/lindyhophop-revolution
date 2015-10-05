4ad873fecf237a732f0ca0bf4283188649e7bf5a
Kim Leung <khl291@gmail.com>
2015-10-05 19:03:32 -0400

    added carousel pink colors

--- browser/js/chooseSong/chooseSong.js
+++ browser/js/chooseSong/chooseSong.js
@@ -11,24 +11,16 @@ app.config(function ($stateProvider) {
 app.controller('ChooseSongCtrl', function ($scope, AuthService, $state) {
     console.log("hey Kim")
     // set and cache variables
-		$scope.w;
-    $scope.container;
-    $scope.carousel;
-    $scope.item;
-    $scope.radius;
-    $scope.itemLength;
-    $scope.rY;
-    $scope.ticker;
-    $scope.fps;
-		$scope.mouseX = 0;
-		$scope.mouseY = 0;
-		$scope.mouseZ = 0;
-		$scope.addX = 0;
+		var w, container, carousel, item, radius, itemLength, rY, ticker, fps;
+		var mouseX = 0;
+		var mouseY = 0;
+		var mouseZ = 0;
+		var addX = 0;
 
 
 		// fps counter created by: https://gist.github.com/sharkbrainguy/1156092,
 		// no need to create my own :)
-		$scope.fps_counter = {
+		var fps_counter = {
 
 			tick: function ()
 			{
@@ -49,58 +41,56 @@ app.controller('ChooseSongCtrl', function ($scope, AuthService, $state) {
 			times: [],
 			span: 20
 		};
-		$scope.counter = Object.create($scope.fps_counter);
+		var counter = Object.create(fps_counter);
 
 
 
-		// $(document).ready( $scope.init )
+		$(document).ready( init )
 
-		$scope.init = function()
+		function init()
 		{
-			$scope.w = $(window);
-			$scope.container = $( '#contentContainer' );
-			$scope.carousel = $( '#carouselContainer' );
-			$scope.item = $( '.carouselItem' );
-			$scope.itemLength = $( '.carouselItem' ).length;
-			$scope.fps = $('#fps');
-			$scope.rY = 360 / $scope.itemLength;
-			$scope.radius = Math.round( (250) / Math.tan( Math.PI / $scope.itemLength ) );
+			w = $(window);
+			container = $( '#contentContainer' );
+			carousel = $( '#carouselContainer' );
+			item = $( '.carouselItem' );
+			itemLength = $( '.carouselItem' ).length;
+			fps = $('#fps');
+			rY = 360 / itemLength;
+			radius = Math.round( (250) / Math.tan( Math.PI / itemLength ) );
 
-			// set $scope.container 3d props
-			TweenMax.set($scope.container, {perspective:600})
-			TweenMax.set($scope.carousel, {z:-($scope.radius)})
+			// set container 3d props
+			TweenMax.set(container, {perspective:600})
+			TweenMax.set(carousel, {z:-(radius)})
 
-			// create carousel $scope.item props
+			// create carousel item props
 
-			for ( var i = 0; i < $scope.itemLength; i++ )
+			for ( var i = 0; i < itemLength; i++ )
 			{
-        console.log("this is the scope.item.eq: ", $scope.item.eq(i))
-				var $item = $scope.item.eq(i);
+				var $item = item.eq(i);
 				var $block = $item.find('.carouselItemInner');
-        console.log("this is the block: ", $block)
 
         //thanks @chrisgannon!
-        TweenMax.set($item, {rotationY:$scope.rY * i, z:$scope.radius, transformOrigin:"50% 50% " + -$scope.radius + "px"});
+        TweenMax.set($item, {rotationY:rY * i, z:radius, transformOrigin:"50% 50% " + -radius + "px"});
 
-				$scope.animateIn( $item, $block )
+				animateIn( $item, $block )
 			}
 
-			// set mouse x and y props and looper $scope.ticker
-			window.addEventListener( "mousemove", $scope.onMouseMove, false );
-			$scope.ticker = setInterval( $scope.looper, 1000/60 );
+			// set mouse x and y props and looper ticker
+			window.addEventListener( "mousemove", onMouseMove, false );
+			ticker = setInterval( looper, 1000/60 );
 		}
 
-		$scope.animateIn = function( $item, $block )
+		function animateIn( $item, $block )
 		{
-			var $nrX = 360 * $scope.getRandomInt(2);
-			var $nrY = 360 * $scope.getRandomInt(2);
+			var $nrX = 360 * getRandomInt(2);
+			var $nrY = 360 * getRandomInt(2);
 
-			var $nx = -(2000) + $scope.getRandomInt( 4000 )
-			var $ny = -(2000) + $scope.getRandomInt( 4000 )
-			var $nz = -4000 +  $scope.getRandomInt( 4000 )
+			var $nx = -(2000) + getRandomInt( 4000 )
+			var $ny = -(2000) + getRandomInt( 4000 )
+			var $nz = -4000 +  getRandomInt( 4000 )
 
-			var $s = 1.5 + ($scope.getRandomInt( 10 ) * .1)
-			var $d = 1 - ($scope.getRandomInt( 8 ) * .1)
+			var $s = 1.5 + (getRandomInt( 10 ) * .1)
+			var $d = 1 - (getRandomInt( 8 ) * .1)
 
 			TweenMax.set( $item, { autoAlpha:1, delay:$d } )
 			TweenMax.set( $block, { z:$nz, rotationY:$nrY, rotationX:$nrX, x:$nx, y:$ny, autoAlpha:0} )
@@ -108,25 +98,24 @@ app.controller('ChooseSongCtrl', function ($scope, AuthService, $state) {
 			TweenMax.to( $block, $s-.5, { delay:$d, x:0, y:0, autoAlpha:1, ease:Expo.easeInOut} )
 		}
 
-		$scope.onMouseMove = function(event)
+		function onMouseMove(event)
 		{
-			$scope.mouseX = -(-(window.innerWidth * .5) + event.pageX) * .0025;
-			$scope.mouseY = -(-(window.innerHeight * .5) + event.pageY ) * .01;
-			$scope.mouseZ = -($scope.radius) - (Math.abs(-(window.innerHeight * .5) + event.pageY ) - 200);
+			mouseX = -(-(window.innerWidth * .5) + event.pageX) * .0025;
+			mouseY = -(-(window.innerHeight * .5) + event.pageY ) * .01;
+			mouseZ = -(radius) - (Math.abs(-(window.innerHeight * .5) + event.pageY ) - 200);
 		}
 
 		// loops and sets the carousel 3d properties
-		$scope.looper = function()
+		function looper()
 		{
-			$scope.addX += $scope.mouseX
-			TweenMax.to( $scope.carousel, 1, { rotationY:$scope.addX, rotationX:$scope.mouseY, ease:Quint.easeOut } )
-			TweenMax.set( $scope.carousel, {z:$scope.mouseZ } )
-			$scope.fps.text( 'Framerate: ' + $scope.counter.tick() + '/60 FPS' )
+			addX += mouseX
+			TweenMax.to( carousel, 1, { rotationY:addX, rotationX:mouseY, ease:Quint.easeOut } )
+			TweenMax.set( carousel, {z:mouseZ } )
+			fps.text( 'Framerate: ' + counter.tick() + '/60 FPS' )
 		}
 
-		$scope.getRandomInt = function( $n )
+		function getRandomInt( $n )
 		{
 			return Math.floor((Math.random()*$n)+1);
 		}
-    $scope.init();
 });
