20f60601497bbf6b7ea0748cc3d17e38486c492b
Jimin Sung <jiminsung27@gmail.com>
2015-10-08 12:12:10 -0400

    home selection not working

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -25,4 +25,35 @@ app.controller('HomeController', function($rootScope, $scope, AuthService, AUTH_
   $rootScope.$on(AUTH_EVENTS.loginSuccess, setUser);
   $rootScope.$on(AUTH_EVENTS.logoutSuccess, removeUser);
 
+  window.addEventListener('keydown', onArrowKey);
+
+  function play() {
+    var audio = document.getElementById("blop");
+    audio.play();
+  };
+
+  function onArrowKey(event) {
+      var active = $('.activeHome');
+      var activeNumber = parseInt(active[0].id.slice(-1));
+
+    if(event.which === 39) { //right
+      play();
+      activeNumber = activeNumber === 5? 1 : activeNumber + 1;
+      active.removeClass("activeHome");
+      $('#option' + activeNumber).addClass("activeHome");
+    } else if(event.which === 37) { //left
+      play();
+      activeNumber = activeNumber === 1? 5 : activeNumber - 1;
+      active.removeClass("activeHome");
+      $('#option' + activeNumber).addClass("activeHome");
+    } else if(event.keyCode === 13) {
+      var uiState = active[0].outerHTML.split('"');
+      window.removeEventListener('keydown', onArrowKey);
+      $state.go(uiState[5]);
+    } else if(event.keyCode === 27) {
+      window.removeEventListener('keydown', onArrowKey);
+      $state.go('home');
+    };
+  };
+
 });
