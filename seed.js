84ce7318c702b4210dc1f5c1490f6cfb0230b84a
jl975 <jlee6891@gmail.com>
2015-10-10 16:52:17 -0400

    update post route and seed for extra fields on song model

--- seed.js
+++ seed.js
@@ -79,6 +79,10 @@ function createStepCharts(parsedSM) {
                 displayBpm: parsedSM.metadata.DISPLAYBPM,
                 offset: parsedSM.metadata.OFFSET,
                 music: parsedSM.metadata.MUSIC,
+                sampleStart: parsedSM.metadata.SAMPLESTART,
+                sampleLength: parsedSM.metadata.SAMPLELENGTH,
+                banner: parsedSM.metadata.BANNER,
+                background: parsedSM.metadata.BACKGROUND,
                 Charts: chartsObj
             });
 
